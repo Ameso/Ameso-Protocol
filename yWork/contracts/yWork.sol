ea4aadfe25b16f359f1e7898b2566a83fb20b2dd
simon145 <simon.liu@mountsinai.org>
2020-12-31 15:04:42 -0500

    Initial yWork core contract

---
+++ yWork/contracts/yWork.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.7.0;
+
+contract yWork {
+	address treasury;
+    
+    constructor(address _treasury) public {
+
+    }
+
+    /**
+     * @dev Review calls this function to approve/disapprove job done by contractor
+     */
+    function reviewJob(JobListing id, bool approve) public {
+
+    }
+}

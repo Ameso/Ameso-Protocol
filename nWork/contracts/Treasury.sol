9564295d63245a273cebc74e5d22d25989109493
simon <sim.liu92@gmail.com>
2021-01-11 17:33:42 -0500

    Make Treasury inherit from Timelock

--- nWork/contracts/Treasury.sol
+++ nWork/contracts/Treasury.sol
@@ -2,56 +2,14 @@
 pragma solidity ^0.7.0;
 
 import '@openzeppelin/contracts/math/SafeMath.sol';
+import './Timelock.sol';
 
-contract Treasury {
+contract Treasury is Timelock{
 
     using SafeMath for uint256;
 
-    event NewAdmin(address indexed newAdmin);
-    event NewPendingAdmin(address indexed newPendingAdmin);
+    constructor() Timelock() {
 
-    // -- State --
-    uint256 public constant GRACE_PERIOD = 14 days;
-    uint256 public constant MINIMUM_DELAY = 2 days;
-    uint256 public constant MAXIMUM_DELAY = 30 days;
-    address public admin;
-
-    // if we ever want to change the governance contract
-    address public pendingAdmin;
-
-    mapping (address => uint256) stakingBalances;
-
-    uint32 public thawingPeriod;
-
-    /**
-     * @param _admin The governance contract
-     * @param _delay Delay before we can deploy. Used to allow dependencies to deploy first.
-     */
-    constructor(address _admin, uint256 _delay) {
-        require(_delay >= MINIMUM_DELAY, "Treasury:constructor: Delay must exceed minimum delay");
-        require(_delay <= MAXIMUM_DELAY, "Treasury:constructor: Delay must not exceed maximum delay");
-        admin = _admin;
     }
 
-    function acceptAdmin() public {
-        require(msg.sender == pendingAdmin, "Treasury::acceptAdmin: Call must come from pendingAdmin.");
-        admin = msg.sender;
-        pendingAdmin = address(0);
-
-        emit NewAdmin(admin);
-    }
-
-    function setPendingAdmin(address _pendingAdmin) public {
-        require(msg.sender == address(this), "Treasury::setPendingAdmin: Call must come from Treasury.");
-        pendingAdmin = _pendingAdmin; 
-
-        emit NewPendingAdmin(pendingAdmin);
-    }
-
-    /**
-     * @dev The treasury will execute functions only if the Governance contract allows of it
-     */
-    function executeTransaction() public {
-        require(msg.sender == admin, "Treasury::executeTransaction: Call must come from admin."); 
-    }
 }

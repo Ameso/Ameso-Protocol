16983f27e920e7843b3a62efd769d8e42a6cfc38
simon <sim.liu92@gmail.com>
2021-01-10 20:16:33 -0500

    Removed deploy script for now. Also added more tests and cleaned up structure of tests

--- nWork/test/nWorkToken.js
+++ nWork/test/nWorkToken.js
@@ -3,158 +3,180 @@ const truffleAssert = require('truffle-assertions');
 const { toBN, advanceBlock, takeSnapshot, revertToSnapShot, getCurrentEpochTime, DEVAMT, TREASURYAMT } = require('./utils.js');
  
 
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
 contract("nWorkToken", async addresses => {
     const [admin, user1, user2, user3, user4, _] = addresses;
     let nWorkInstance = null;
     let minter;
     
-    it('Can deploy nWork token', async () => {
-        minter = user2; 
-        nWorkInstance = await nWorkToken.new(user1, minter, DEVAMT, TREASURYAMT, getCurrentEpochTime())
-        let totalSupply = await nWorkInstance.totalSupply();
-        assert(totalSupply.toString() === DEVAMT.add(TREASURYAMT).toString(), "Incorrect total supply: " + totalSupply.toString());
-    });
+    describe('Basic deployment', async () => {    
+        it('Can deploy nWork token', async () => {
+            minter = user2; 
+            nWorkInstance = await nWorkToken.new(user1, minter, DEVAMT, TREASURYAMT, getCurrentEpochTime())
+            let totalSupply = await nWorkInstance.totalSupply();
+            assert(totalSupply.toString() === DEVAMT.add(TREASURYAMT).toString(), `Incorrect total supply: ${totalSupply.toString()}`);
+        })
+    })
 
-    it('Proper minting permissions and test basic minting', async () => {
-        let snapShotId = await takeSnapshot();
+    describe('Minting', async () => {
+        it('Proper minting permissions and test basic minting', async () => {
+            let snapShotId = await takeSnapshot();
 
-        // advance blockchain time so we can start minting (check allowmintafter time in contract)
-        await advanceBlock();
+            // advance blockchain time so we can start minting (check allowmintafter time in contract)
+            await advanceBlock();
 
-        newBlock = await web3.eth.getBlock('latest');
+            newBlock = await web3.eth.getBlock('latest');
 
-        // try to mint even though address is not minter
-        // deployer (admin in this case) isn't the one that becomes the minter by default
-        await truffleAssert.reverts(
-       	    nWorkInstance.mint(user1, 10, {from: admin}),
-            "NWK::mint: only the treasury can mint"
-        ); 
+            // try to mint even though address is not minter
+            // deployer (admin in this case) isn't the one that becomes the minter by default
+            await truffleAssert.reverts(
+                nWorkInstance.mint(user1, 10, {from: admin}),
+                "NWK::mint: only the treasury can mint"
+            ); 
 
-        await nWorkInstance.mint(user2, 1, {from: minter});
-        let newTreasuryBalance = await nWorkInstance.balanceOf(user2);
-        assert(newTreasuryBalance.toString() === TREASURYAMT.add(toBN(1)).toString(), "Incorrect treasury balance: " + newTreasuryBalance.toString());
+            await nWorkInstance.mint(user2, 1, {from: minter});
+            let newTreasuryBalance = await nWorkInstance.balanceOf(user2);
+            assert(newTreasuryBalance.toString() === TREASURYAMT.add(toBN(1)).toString(), `Incorrect treasury balance: ${newTreasuryBalance.toString()}`);
 
-        // should not allow mint again. Too soon
-        await truffleAssert.reverts(
-            nWorkInstance.mint(user2, 1, {from: minter}),
-            "NWK::mint: minting not allowed yet"
-        )
+            // should not allow mint again. Too soon
+            await truffleAssert.reverts(
+                nWorkInstance.mint(user2, 1, {from: minter}),
+                "NWK::mint: minting not allowed yet"
+            )
 
-        await revertToSnapShot(snapShotId.result);
-    });
+            await revertToSnapShot(snapShotId.result);
+        });
 
-    it('Cannot mint too much tokens', async () => {
-        let snapShotId = await takeSnapshot();
+        it('Cannot mint too much tokens', async () => {
+            let snapShotId = await takeSnapshot();
 
-        // should not be able to mint past 2 percent
-        await truffleAssert.reverts(
-            nWorkInstance.mint(user2, DEVAMT.add(TREASURYAMT).mul(toBN(2)).div(toBN(100)).add(toBN(1)), {from: minter}),
-            "NWK::mint: exceeded mint cap"
-        );
-        
-        await nWorkInstance.mint(user2, DEVAMT.add(TREASURYAMT).mul(toBN(2)).div(toBN(100)), {from: minter});
+            // should not be able to mint past 2 percent
+            await truffleAssert.reverts(
+                nWorkInstance.mint(user2, DEVAMT.add(TREASURYAMT).mul(toBN(2)).div(toBN(100)).add(toBN(1)), {from: minter}),
+                "NWK::mint: exceeded mint cap"
+            );
+            
+            await nWorkInstance.mint(user2, DEVAMT.add(TREASURYAMT).mul(toBN(2)).div(toBN(100)), {from: minter});
 
-        await revertToSnapShot(snapShotId.result);
-    });
+            await revertToSnapShot(snapShotId.result);
+        });
 
-    it('Can change the minter', async () => {
-        let snapShotId = await takeSnapshot();
+        it('Can change the minter', async () => {
+            let snapShotId = await takeSnapshot();
 
-        await nWorkInstance.setMinter(user3, {from: minter});
+            await nWorkInstance.setMinter(user3, {from: minter});
 
-        await truffleAssert.reverts(
-            nWorkInstance.mint(user2, 100, {from: minter}),
-            "NWK::mint: only the treasury can mint"
-        )
+            await truffleAssert.reverts(
+                nWorkInstance.mint(user2, 100, {from: minter}),
+                "NWK::mint: only the treasury can mint"
+            )
 
-        await nWorkInstance.mint(user2, 100, {from: user3});
+            await nWorkInstance.mint(user2, 100, {from: user3});
 
-        // user3 should still have 0 and user2 should have all balance
-        let user3TreasuryBal = await nWorkInstance.balanceOf(user3);
-        let user2TreasuryBal = await nWorkInstance.balanceOf(user2);
+            // user3 should still have 0 and user2 should have all balance
+            let user3TreasuryBal = await nWorkInstance.balanceOf(user3);
+            let user2TreasuryBal = await nWorkInstance.balanceOf(user2);
 
-        assert(user3TreasuryBal.toString() === "0", "User3 incorrect balance: " + user3TreasuryBal.toString());
-        assert(user2TreasuryBal.toString() === TREASURYAMT.add(toBN(100)).toString(), "User2 incorrect balance: " + user2TreasuryBal.toString());
+            assert(user3TreasuryBal.toString() === "0", `User3 incorrect balance: ${user3TreasuryBal.toString()}`);
+            assert(user2TreasuryBal.toString() === TREASURYAMT.add(toBN(100)).toString(), "User2 incorrect balance: " + user2TreasuryBal.toString());
 
-        await revertToSnapShot(snapShotId.result);
-    });
+            await revertToSnapShot(snapShotId.result);
+        });
 
-    it('User can set allowance to -1 to indicate infinite spending', async () => {
+    })
 
-    });
+    describe('Allowance without permit', async () => {
+        it('User can set allowance to -1 to indicate infinite spending', async () => {
 
-    it('Simple allowance to allow another user to spend my tokens', async () => {
-        let thousandNWKtokens = web3.utils.toWei('1000', 'ether');
+        })
 
-        await nWorkInstance.approve(user3, thousandNWKtokens, {from: user1});
+        it('Simple allowance to allow another user to spend my tokens', async () => {
+            let thousandNWKtokens = web3.utils.toWei('1000', 'ether');
 
-        let allowedAmount = await nWorkInstance.allowance(user1, user3);
+            await nWorkInstance.approve(user3, thousandNWKtokens, {from: user1});
 
-        let user1Bal = await nWorkInstance.balanceOf(user1);
+            let allowedAmount = await nWorkInstance.allowance(user1, user3);
 
-        assert(allowedAmount.toString() === thousandNWKtokens, "Incorrect allowance amount: " + allowedAmount.toString())
+            let user1Bal = await nWorkInstance.balanceOf(user1);
 
-        // once approved, user3 should be able to spend tokens.
-        // only the recipient (user3) should be able to execute the transferFrom
-        await truffleAssert.fails(
-            nWorkInstance.transferFrom(user1, user3, thousandNWKtokens, {from: admin})
-        )
-        await nWorkInstance.transferFrom(user1, user3, thousandNWKtokens, {from: user3});
+            assert(allowedAmount.toString() === thousandNWKtokens, `Incorrect allowance amount: ${allowedAmount.toString()}`)
 
-        let user3NewBal = await nWorkInstance.balanceOf(user3);
+            // once approved, user3 should be able to spend tokens.
+            // only the recipient (user3) should be able to execute the transferFrom
+            await truffleAssert.fails(
+                nWorkInstance.transferFrom(user1, user3, thousandNWKtokens, {from: admin})
+            )
+            await nWorkInstance.transferFrom(user1, user3, thousandNWKtokens, {from: user3});
 
-        assert(user3NewBal.toString() === thousandNWKtokens, "User3 does not have the correct balance: " + user3NewBal.toString());
+            let user3NewBal = await nWorkInstance.balanceOf(user3);
 
-        let user1NewBal = await nWorkInstance.balanceOf(user1);
+            assert(user3NewBal.toString() === thousandNWKtokens, `User3 does not have the correct balance: ${user3NewBal.toString()}`);
 
-        assert(user1NewBal.toString() === DEVAMT.sub(toBN(thousandNWKtokens)).toString(), "User1 did not have their balance removed: " + user1NewBal.toString());
-    });
+            let user1NewBal = await nWorkInstance.balanceOf(user1);
 
-    it('What happens with negative allowance', async () => {
+            assert(user1NewBal.toString() === DEVAMT.sub(toBN(thousandNWKtokens)).toString(), `User1 did not have their balance removed: ${user1NewBal.toString()}`);
+        })
 
-    });
+        it('User should not be able to spend more than allowed tokens', async () => {
+        })   
 
-    it('User should not be able to spend more than allowed tokens', async () => {
-    });
+        it('What happens with negative allowance', async () => {
 
-    it('Use the permit function to allow another address to spend my tokens', async () => {
+        })
+    })
+   
+    describe('Allowance with permit', async () => {
+        it('Use the permit function to allow another address to spend my tokens', async () => {
 
-    });
+        })
+    }) 
 
-    it('Minting should add checkpoint', async () => {
-        let snapShotId = await takeSnapshot()
+    describe('Delegation', async () => {
+        it('Delegating (non mint)', async () => {
+            let snapShotId = await takeSnapshot()
 
-        let beforeNumChkPnt = await nWorkInstance.numCheckpoints(minter)
+            let tx = await nWorkInstance.delegate(admin, {from: user2})
 
-        await nWorkInstance.mint(minter, 99, {from: minter}) 
-        
-        let numChkPnt = await nWorkInstance.numCheckpoints(minter)
-        console.log(beforeNumChkPnt, numChkPnt);
+            truffleAssert.eventEmitted(tx, 'DelegateChanged', (ev) => {
+                return ev.delegator == user2 && ev.fromDelegate == 0 && ev.toDelegate == admin
+            })
 
-        assert(beforeNumChkPnt.add(toBN(1)).toString() === numChkPnt.toString(), 
-            'Incorrect number of checkpoints after mint, before: ' + beforeNumChkPnt.toString() + ' after: ' + numChkPnt.toString()
-        )
+            let myDelegate = await nWorkInstance.delegates(user2)
 
-        await revertToSnapShot(snapShotId.result)
-    });
+            assert(myDelegate === admin, `Incorrect delegator, return : ${myDelegate} , should be: ${admin}`) 
 
-    it('Try get prior votes', async () => {
-        newBlock = await web3.eth.getBlock('latest')
+            // Checkpoint created from delegating
+            let adminNumChkPt = await nWorkInstance.numCheckpoints(admin) 
+            assert(adminNumChkPt.toString() === "1", `Incorrect number of checkpoints : ${adminNumChkPt.toString()}`)
 
-        await truffleAssert.reverts(
-            nWorkInstance.getPriorVotes(user2, newBlock.number),
-            "NWK::getPriorVotes: not yet determined"
-        )
+            await revertToSnapShot(snapShotId.result)
+        })
 
-        let numVotes = await nWorkInstance.getPriorVotes(user2, newBlock.number - 1)
-        assert(numVotes.toString() == "0", "Should be 0 initially. Received: " + numVotes.toString())
+        it('Delegating (after mint)', async () => {
+            let beforeNumChkPnt = await nWorkInstance.numCheckpoints(minter)
 
-        let res = await nWorkInstance.numCheckpoints(user2)
-        let res2 = await nWorkInstance.checkpoints(user2, 0)
-    });
-});
+            await nWorkInstance.mint(minter, 99, {from: minter}) 
+		
+            let numChkPnt = await nWorkInstance.numCheckpoints(minter)
+
+
+        });
+
+    })
+
+    describe('Voting', async () => {
+        it('Try get prior votes', async () => {
+            newBlock = await web3.eth.getBlock('latest')
+
+            await truffleAssert.reverts(
+                nWorkInstance.getPriorVotes(user2, newBlock.number),
+                "NWK::getPriorVotes: not yet determined"
+            )
+
+            let numVotes = await nWorkInstance.getPriorVotes(user2, newBlock.number - 1)
+            assert(numVotes.toString() == "0", `Should be 0 initially. Received: ${numVotes.toString()}`)
+
+            let res = await nWorkInstance.numCheckpoints(user2)
+            let res2 = await nWorkInstance.checkpoints(user2, 0)
+        })
+    }) 
+})
